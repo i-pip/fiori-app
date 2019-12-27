dcd407b52ef9faed0806a2f4441195ea33a8f261
pip <jrphiri@gmail.com>
2019-12-27 10:40:52 +0200

    initial commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,30 +1,35 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"pip/fiori-app/model/models"
-], function(UIComponent, Device, models) {
-	"use strict";
+sap.ui.define(
+	["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
+	function(UIComponent, JSONModel) {
+		"use strict";
 
-	return UIComponent.extend("pip.fiori-app.Component", {
+		return UIComponent.extend("pip.fiori-app.Component", {
+			metadata: {
+				manifest: "json"
+			},
 
-		metadata: {
-			manifest: "json"
-		},
+			/**
+			 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+			 * @public
+			 * @override
+			 */
+			init: function() {
+				var oProductsModel;
+				// call the base component's init function
+				UIComponent.prototype.init.apply(this, arguments);
 
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function() {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+				// enable routing
+				this.getRouter().initialize();
 
-			// enable routing
-			this.getRouter().initialize();
+				// set products demo model on this sample
+				oProductsModel = new JSONModel(
+					sap.ui.require.toUrl("pip/fiori-app/data") + "/products.json"
+				);
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
-});
+				oProductsModel.setSizeLimit(1000);
+
+				this.setModel(oProductsModel, "products");
+			}
+		});
+	}
+);
